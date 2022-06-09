e438d612da1732d9d52a3178386a18c674b2d1c2
wolfishLamb <jaredliw@gmail.com>
2022-06-09 11:27:02 +0800

    Install vuetify and vue-avatar

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,8 +3,10 @@ import VueAxios from "vue-axios";
 import VueMeta from "vue-meta";
 import VueSweetalert2 from "vue-sweetalert2";
 import axios from "axios";
-import App from "./App.vue";
+
+import App from "./App";
 import router from "./router";
+import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
 Vue.use(VueMeta);
@@ -18,5 +20,6 @@ Vue.use(VueSweetalert2, {
 
 new Vue({
   router,
+  vuetify,
   render: (h) => h(App),
 }).$mount("#app");
