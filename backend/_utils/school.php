4fc1ecb9eb341d1a72fd03bfaa30d8b3e2a970e8
wolfishLamb <jaredliw@gmail.com>
2022-06-16 23:12:59 +0800

    Add one more attr: student_count, using subquery to join two tables

--- backend/_utils/school.php
+++ backend/_utils/school.php
@@ -5,8 +5,16 @@ function fetch_all_schools(): array
 {
     # Return all schools
     $stmt = MySQL::connection()->prepare("
-        SELECT *
-        FROM school;
+        SELECT s.code,
+               s.name,
+               COUNT(u.username) AS student_count
+        FROM   school s
+               LEFT JOIN (SELECT username,
+                                 school
+                          FROM   user
+                          WHERE  user.role = 'student') u
+                      ON u.school = s.code
+        GROUP  BY s.code; 
     ");
     $stmt->execute();
     return $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
