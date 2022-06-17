52ef21d95b8ce70dfdd80e7c9fe0dc91eeef6ab9
wolfishLamb <jaredliw@gmail.com>
2022-06-17 12:39:41 +0800

    Impl /admin/contest DELETE method

--- backend/_utils/contest.php
+++ backend/_utils/contest.php
@@ -1,6 +1,28 @@
 <?php
 require_once __DIR__ . "/database.php";
 
+function check_contest_exists(string $id, bool $expect_to_be): void
+{
+    $stmt = MySQL::connection()->prepare("
+        SELECT id
+        FROM contest
+        WHERE id = ?
+    ");
+    $stmt->bind_param("s", $id);
+    $stmt->execute();
+
+    if ($expect_to_be xor $stmt->get_result()->num_rows !== 0) {
+        json_write(
+            $expect_to_be ? 404 : 409,
+            'Pertandingan dengan kod \'' .
+            htmlspecialchars($id) .
+            '\' ' .
+            ($expect_to_be ? "tidak" : "telah") .
+            " wujud dalam pangkalan data."
+        );
+    }
+}
+
 function fetch_all_contests(): array
 {
     # Return all schools
@@ -10,4 +32,14 @@ function fetch_all_contests(): array
     ");
     $stmt->execute();
     return $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+}
+
+function delete_contest($id): void
+{
+    $stmt = MySQL::connection()->prepare("
+        DELETE FROM contest
+        WHERE id = ?;
+    ");
+    $stmt->bind_param("s", $id);
+    $stmt->execute();
 }
---
+++ backend/admin/contest.delete.php
@@ -0,0 +1,14 @@
+<?php
+require_once __DIR__ . "/../_utils/security.php";
+check_access("admin");
+
+require_once __DIR__ . "/../_utils/io.php";
+require_once __DIR__ . "/../_utils/contest.php";
+
+$json = json_read();
+$contest_id = compulsory_param($json->id);
+
+check_contest_exists($contest_id, true);
+delete_contest($contest_id);
+
+json_write(200, "Pertandingan dengan kod '" . $contest_id . "' telah dihapuskan.");
