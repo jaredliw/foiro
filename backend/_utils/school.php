50943ea35127e4b8b6a9aa1f72f81afaabe3d781
wolfishLamb <jaredliw@gmail.com>
2022-06-17 12:52:01 +0800

    Impl /api/admin/school DELETE method

--- backend/_utils/school.php
+++ backend/_utils/school.php
@@ -1,6 +1,28 @@
 <?php
 require_once __DIR__ . "/database.php";
 
+function check_school_exists(string $code, bool $expect_to_be): void
+{
+    $stmt = MySQL::connection()->prepare("
+        SELECT code
+        FROM school
+        WHERE code = ?
+    ");
+    $stmt->bind_param("s", $code);
+    $stmt->execute();
+
+    if ($expect_to_be xor $stmt->get_result()->num_rows !== 0) {
+        json_write(
+            $expect_to_be ? 404 : 409,
+            'Sekolah degan kod \'' .
+            htmlspecialchars($code) .
+            '\' ' .
+            ($expect_to_be ? "tidak" : "telah") .
+            " wujud dalam pangkalan data."
+        );
+    }
+}
+
 function fetch_all_schools(): array
 {
     # Return all schools
@@ -18,4 +40,14 @@ function fetch_all_schools(): array
     ");
     $stmt->execute();
     return $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+}
+
+function delete_school($code): void
+{
+    $stmt = MySQL::connection()->prepare("
+        DELETE FROM school
+        WHERE code = ?;
+    ");
+    $stmt->bind_param("s", $code);
+    $stmt->execute();
 }
---
+++ backend/admin/school.delete.php
@@ -0,0 +1,14 @@
+<?php
+require_once __DIR__ . "/../_utils/security.php";
+check_access("admin");
+
+require_once __DIR__ . "/../_utils/io.php";
+require_once __DIR__ . "/../_utils/school.php";
+
+$json = json_read();
+$school_code = compulsory_param($json->code);
+
+check_school_exists($school_code, true);
+delete_school($school_code);
+
+json_write(200, "Sekolah dengan kod '" . $school_code . "' telah dihapuskan.");
