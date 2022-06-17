8f583abdbc7a9ac6ce3d49d8f840111f92d07e19
wolfishLamb <jaredliw@gmail.com>
2022-06-17 14:32:35 +0800

    impl /me according to new db structure

--- backend/me.get.php
+++ backend/me.get.php
@@ -1,7 +1,8 @@
 <?php
 require_once __DIR__ . "/_utils/database.php";
 require_once __DIR__ . "/_utils/io.php";
-require_once __DIR__ . "/_utils/user.php";
+require_once __DIR__ . "/_utils/student.php";
+require_once __DIR__ . "/_utils/judge.php";
 
 session_start();
 $my_username = $_SESSION["username"];
@@ -9,14 +10,28 @@ if (!$my_username) {
     json_write(401, "Sila log masuk terlebih dahulu.");
 }
 
-$user = search_user_on_username($my_username);
-if (is_null($user)) {
+$role = $_SESSION["role"];
+switch ($role) {
+    case "student":
+        $user = search_student_on_username($my_username);
+        break;
+    case "judge":
+        $user = search_judge_on_username($my_username);
+        break;
+    case "admin":
+        # todo, implement
+        $user = [
+            "username" => "testing",
+            "name" => "Testing"
+        ];
+        break;
+    default:
+        $user = null;
+}
+
+if ($user === null) {
     logout();
     json_write(401, "Sesi pelanggan tamat. Sila log masuk semula.");
 }
 
-json_write(200, "Berjaya.", [
-    "username" => $user["username"],
-    "name" => $user["name"],
-    "role" => $user["role"],
-]);
+json_write(200, "Berjaya.", $user);
